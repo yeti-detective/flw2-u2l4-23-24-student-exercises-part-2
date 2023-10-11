a24d15c06d618e9d7bd2e784caeed47a67adf386
CN-Mika <83977863+CN-Mika@users.noreply.github.com>
2023-10-11 21:55:54 +0000

    Update array

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import './App.css';
 
 // 1. Look over the starter code.
 let photos = [
-  "https://ichef.bbci.co.uk/news/976/cpsprodpb/17638/production/_124800859_gettyimages-817514614.jpg",
+"https://ichef.bbci.co.uk/news/976/cpsprodpb/17638/production/_124800859_gettyimages-817514614.jpg",
   "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_2x1.jpg",
   "https://cf.ltkcdn.net/small-mammals/guinea-pigs/images/orig/326593-2119x1413-sheltie-guinea-pig.jpg"
 ];
+
 let counter = 0;
 
 
